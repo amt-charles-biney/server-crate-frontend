93487ee78a3460f4fd3a967482a12a0315c15283
Abdul-Raziq Wumpini Baba <abdul-raziq.baba@amalitech.com>
2023-12-09 10:53:23 +0000

    lazy loaded signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { SignUpComponent } from './features/sign-up/sign-up.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'signup',
+        loadChildren: () => import('./features/sign-up/sign-up.routes').then(m => m.route)
+        // component: SignUpComponent
+    }
+];
