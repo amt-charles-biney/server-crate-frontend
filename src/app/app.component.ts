f4ad059ea343d00bb9605ad691f95e7dc6af2a70
Abdul-Raziq Wumpini Baba <abdul-raziq.baba@amalitech.com>
2023-12-11 22:03:00 +0000

    styling: Styling the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { FooterComponent } from './core/components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
