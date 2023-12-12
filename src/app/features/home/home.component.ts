91ad97f462995ebcd58d5d4b7e32d40a3a721cf2
Abdul-Raziq Wumpini Baba <abdul-raziq.baba@amalitech.com>
2023-12-12 10:02:49 +0000

    feat: Added header

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../core/components/footer/footer.component';
+import { HeaderComponent } from '../../core/components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
