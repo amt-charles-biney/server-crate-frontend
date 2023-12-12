1ac650ba2f37210ab522912b99cc62b2c914b832
Abdul-Raziq Wumpini Baba <abdul-raziq.baba@amalitech.com>
2023-12-12 14:07:49 +0000

    feat: Adding profile elements

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -1,3 +1,4 @@
+import { AccountSettingsComponent } from '../account-settings/account-settings.component';
 import { HomeComponent } from './home.component';
 import { Routes } from "@angular/router";
 
@@ -6,8 +7,10 @@ export const route: Routes = [
         path: '',
         children: [
             {
-                path: '', component: HomeComponent
+                path: 'settings',
+                loadChildren: () => import('../account-settings/account-settings.routes').then(m => m.route)
             }
-        ]
+        ],
+        component: HomeComponent
     }
 ]
