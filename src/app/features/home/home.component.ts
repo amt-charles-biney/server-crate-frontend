9030024b02b4b49991f4e1c88be88f8353ce9521
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-06 20:50:24 +0000

    feat(minor): added landing page module to hero component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,12 @@ import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../core/components/footer/footer.component';
 import { HeaderComponent } from '../../core/components/header/header.component';
+import { LandingComponent } from '../landing/landing.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, FooterComponent, HeaderComponent],
+  imports: [RouterModule, FooterComponent, HeaderComponent, LandingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
