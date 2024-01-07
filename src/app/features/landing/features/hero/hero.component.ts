6bc37865afda5083648e747f19b7a127ee14db61
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-07 09:04:59 +0000

    feat: hero slider functionality

--- src/app/features/landing/features/hero/hero.component.ts
+++ src/app/features/landing/features/hero/hero.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { heroSlider } from './hero.interface';
+import { CommonModule } from '@angular/common';
+import KeenSlider, { KeenSliderInstance } from 'keen-slider'
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrls: [
+    "../../../../../../node_modules/keen-slider/keen-slider.min.css",
+    "./hero.component.scss",
+  ],
 })
 export class HeroComponent {
+
+  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement> | any
+  
+  currentSlide: number = 0
+  dotHelper: Array<Number> = []
+  slider: any;
+
+  sliders: heroSlider[] = [{
+    img: "/assets/hero1.jpg",
+    text: "high quality work",
+    subtext: "help"
+  },
+  {
+    img: "/assets/hero2.jpg",
+    text: "high quality work",
+    subtext: "help"
+  }, 
+  {
+    img: "/assets/hero3.jpg",
+    text: "high quality work",
+    subtext: "help"
+  }
+]
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        initial: this.currentSlide,
+        slideChanged: (s) => {
+          this.currentSlide = s.track.details.rel
+        },
+      })
+      this.dotHelper = [
+        ...Array(this.slider.track.details.slides.length).keys(),
+      ]
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
+
+
 
 }
