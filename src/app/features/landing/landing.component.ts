9f2493bb2347d2c87951b8a98eec21b2e01ddeb2
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-07 10:58:34 +0000

    feat(minor): add About feature done to landing page

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './features/hero/hero.component';
 import { BannerComponent } from './features/banner/banner.component';
 import { FeaturedProductsComponent } from './features/featured-products/featured-products.component';
+import { AboutComponent } from './features/about/about.component';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, BannerComponent, FeaturedProductsComponent],
+  imports: [HeroComponent, BannerComponent, FeaturedProductsComponent, AboutComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
