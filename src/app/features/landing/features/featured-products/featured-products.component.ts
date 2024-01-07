69e8d4e8b83798317f9e7bea8a0d965b459df805
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-07 09:48:00 +0000

    feat: added base featured products component to landing page

---
+++ src/app/features/landing/features/featured-products/featured-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-products.component.html',
+})
+export class FeaturedProductsComponent {
+
+}
