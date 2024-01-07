d75244c498bcce7e38725a5c73b15d02089b10a4
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-07 10:09:58 +0000

    feat: added featured product page without featured products

--- src/app/features/landing/features/featured-products/featured-products.component.ts
+++ src/app/features/landing/features/featured-products/featured-products.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { ProductItem } from '../../../../types';
+import { CommonModule } from '@angular/common';
+import { ProductItemComponent } from '../../../../shared/components/product-item/product-item.component';
 
 @Component({
   selector: 'app-featured-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductItemComponent],
   templateUrl: './featured-products.component.html',
 })
 export class FeaturedProductsComponent {
+
+  featuredProducts: ProductItem[] | any = [];
 
 }
