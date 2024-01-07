5d98ebccd56cbe6e1c8e73f39bd8631ea72cd75e
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-07 09:07:52 +0000

    feat(minor): added banner component to landing page

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './features/hero/hero.component';
+import { BannerComponent } from './features/banner/banner.component';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, BannerComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
