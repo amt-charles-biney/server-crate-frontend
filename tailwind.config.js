bcb68c1fa55e461ea671995bcbb4e062a831afa8
MAXIMO MUGISHA <maximo.mugisha@amalitech.org>
2024-01-08 21:31:42 +0300

    add linting to actions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}"
+    './src/**/*.{html,ts}'
   ],
   theme: {
     extend: {
@@ -9,8 +9,7 @@ module.exports = {
         'figma-gray': '#bdbdbd',
         'figma-green': '#3cb043'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
