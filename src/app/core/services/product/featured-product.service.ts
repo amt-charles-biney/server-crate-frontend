d2fb91eb896d0493a351941a5e9e5606d1e1e793
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-09 10:48:40 +0000

    feat: get featured products from backend and display

---
+++ src/app/core/services/product/featured-product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ProductItem } from '../../../types';
+import { environment } from '../../../../environments/environment.development';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeaturedProductService {
+
+  featuredProductUrl: string = `${environment.base_url}/featured`
+
+  constructor(private http: HttpClient) { }
+
+  getFeaturedProducts(): Observable<ProductItem[]> {
+     return this.http.get<ProductItem[]>(this.featuredProductUrl) 
+  }
+}
+
