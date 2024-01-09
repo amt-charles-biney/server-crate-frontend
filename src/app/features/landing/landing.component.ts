5203d4562101e1fe01a4777eda3c262391da2f84
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-09 14:31:09 +0000

    feat: customer feedback done and added base homepage product page

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -4,10 +4,11 @@ import { BannerComponent } from './features/banner/banner.component';
 import { FeaturedProductsComponent } from './features/featured-products/featured-products.component';
 import { AboutComponent } from './features/about/about.component';
 import { PopularProductsComponent } from './features/popular-products/popular-products.component';
+import { FeedbackComponent } from './features/feedback/feedback.component';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, BannerComponent, FeaturedProductsComponent, AboutComponent, PopularProductsComponent],
+  imports: [HeroComponent, BannerComponent, FeaturedProductsComponent, AboutComponent, PopularProductsComponent, FeedbackComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
---
+++ src/app/shared/components/homepage-product-item/homepage-product-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-product-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage-product-item.component.html',
+  styleUrl: './homepage-product-item.component.scss'
+})
+export class HomepageProductItemComponent {
+
+}
