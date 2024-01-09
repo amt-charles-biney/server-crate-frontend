7c1bb8e98e1c8c5183e9f2bc4fe091814d3f0b34
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-09 08:49:48 +0000

    feat: added popular product section

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -3,10 +3,11 @@ import { HeroComponent } from './features/hero/hero.component';
 import { BannerComponent } from './features/banner/banner.component';
 import { FeaturedProductsComponent } from './features/featured-products/featured-products.component';
 import { AboutComponent } from './features/about/about.component';
+import { PopularProductsComponent } from './features/popular-products/popular-products.component';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, BannerComponent, FeaturedProductsComponent, AboutComponent],
+  imports: [HeroComponent, BannerComponent, FeaturedProductsComponent, AboutComponent, PopularProductsComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
