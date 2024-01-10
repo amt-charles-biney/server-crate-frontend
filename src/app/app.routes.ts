2591350242faaf1d48bcab344d6e2e9cc57bf7e9
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-10 07:50:22 -1200

    fix: fix product configur page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState } from '@ngrx/store';
 
-
 export const routes: Routes = [
     {
         path: 'product/configure/:id',
         loadChildren: () => import('./features/product-configure/product-configure.routes').then(m => m.route),
-    }
+        providers: [
+            provideState(productConfigFeature),
+            provideEffects(ProductSpecEffects)
+        ]
+    },
+
 ];
