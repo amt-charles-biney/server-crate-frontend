34b0528da82f0d5bb6dd57ea8dda65cc435505c9
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-10 14:48:48 +0000

    refactor: format popular product with style formatter

--- src/app/features/landing/features/popular-products/popular-products.component.ts
+++ src/app/features/landing/features/popular-products/popular-products.component.ts
@@ -1,10 +1,8 @@
 import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import { ProductItem } from '../../../../types';
 import KeenSlider, { KeenSliderInstance } from 'keen-slider'
-import { ProductItemComponent } from '../../../../shared/components/product-item/product-item.component';
 import { Store } from '@ngrx/store';
 import { loadNewProducts } from '../../../../store/product/featured-product/featured-product.action';
-import { Observable } from 'rxjs';
 import { selectNewProducts } from '../../../../store/product/featured-product/featured-product.reducer';
 import { CommonModule } from '@angular/common';
 import { HomepageProductItemComponent } from '../../../../shared/components/homepage-product-item/homepage-product-item.component';
@@ -24,17 +22,17 @@ export class PopularProductsComponent {
   popularProductSlider!: KeenSliderInstance
   popularProducts$ = this.store.select(selectNewProducts)
   currentPopularSlide = 0
-  
-  constructor(private store: Store, private cdr: ChangeDetectorRef) {}
+
+  constructor(private store: Store, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.store.dispatch(loadNewProducts())
   }
 
-  
+
   ngAfterViewInit(): void {
     setTimeout(() => {
-      if(this.popularSliderRef) {
+      if (this.popularSliderRef) {
         this.popularProductSlider = new KeenSlider(this.popularSliderRef.nativeElement, {
           initial: this.currentPopularSlide,
           loop: true,
@@ -43,22 +41,22 @@ export class PopularProductsComponent {
           },
           mode: "free",
           breakpoints: {
-            "(min-width: 400px)": {  
-              slides: { perView: 2, spacing: 20 },  
+            "(min-width: 400px)": {
+              slides: { perView: 2, spacing: 20 },
             },
-            "(min-width: 640px)": {  
-              slides: { perView: 2, spacing: 20 }, 
+            "(min-width: 640px)": {
+              slides: { perView: 2, spacing: 20 },
             },
-            "(min-width: 1024px)": { 
-              slides: { perView: 3, spacing: 30 }, 
+            "(min-width: 1024px)": {
+              slides: { perView: 3, spacing: 30 },
             },
             "(min-width: 1280px)": {
-              slides: { perView: 3, spacing: 40 },  
+              slides: { perView: 3, spacing: 40 },
             },
           },
           slides: {
-            perView: 1, 
-            spacing: 10, 
+            perView: 1,
+            spacing: 10,
           },
         })
       }
