a55f5f9def2f3088b503cc2ff604a81bd64f29be
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-10 16:10:17 +0000

    refactor: moved landing page to seperate route

--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -1,7 +1,11 @@
+import { provideState } from '@ngrx/store';
 import { authGuard } from '../../core/guards/auth.guard';
 import { settingsGuard } from '../../core/guards/settings.guard';
 import { HomeComponent } from './home.component';
 import { Routes } from "@angular/router";
+import { provideEffects } from '@ngrx/effects';
+import { FeaturedProductEffect } from '../../store/product/featured-product/featured-product.effect';
+import { FeaturedProductFeature } from '../../store/product/featured-product/featured-product.reducer';
 
 export const route: Routes = [
     {
@@ -14,8 +18,16 @@ export const route: Routes = [
                     settingsGuard,
                     authGuard
                 ]
+            },
+            {
+                path: '',
+                loadChildren: () => import('../landing/landing.routes').then(m => m.route),
+                providers: [
+                    provideState(FeaturedProductFeature),
+                    provideEffects(FeaturedProductEffect)
+                ]
             }
         ],
         component: HomeComponent
-    }
+    },
 ]
