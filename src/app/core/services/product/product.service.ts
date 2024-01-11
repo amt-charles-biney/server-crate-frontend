05038dc37c57da9369331f9913ecf735710728fd
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-10 21:57:22 -1200

    refactor: changed url for getting product config to match backend

--- src/app/core/services/product/product.service.ts
+++ src/app/core/services/product/product.service.ts
@@ -8,15 +8,16 @@ import { environment } from '../../../../environments/environment.development';
 })
 export class ProductService {
 
-  productUrl: string = environment.products_user
+  productUserUrl: string = environment.products_user
+  categoryUserUrl: string = environment.category_user
 
   constructor(private http: HttpClient) { }
 
   getProduct(id: string) {
-    return this.http.get<ProductItem>(`${this.productUrl}/${id}`)
+    return this.http.get<ProductItem>(`${this.productUserUrl}/${id}`)
   }
 
   getProductConfiguration(categoryId: string) {
-    return this.http.get<any>(`${this.productUrl}/config/${categoryId}`)
+    return this.http.get<any>(`${this.categoryUserUrl}/${categoryId}/config`)
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,5 +4,6 @@ export const environment = {
     categories: 'http://localhost:8080/api/v1/admin/category',
     brands: 'http://localhost:8080/api/v1/admin/brand',
     products: 'http://localhost:8080/api/v1/admin/product', 
-    products_user: 'http://localhost:8080/api/v1/product'
+    products_user: 'http://localhost:8080/api/v1/product',
+    category_user: 'http://localhost:8080/api/v1/category'
 };
