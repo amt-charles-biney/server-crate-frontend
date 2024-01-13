8ceb5a47f8fade555b5789837a7557b902d976f5
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-13 00:38:38 -1200

    refactor: seperate landing page from home

--- src/app/features/landing/landing.routes.ts
+++ src/app/features/landing/landing.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from "@angular/router";
 import { LandingComponent } from "./landing.component";
+import { provideState } from "@ngrx/store";
+import { provideEffects } from "@ngrx/effects";
+import { FeaturedProductEffect } from "../../store/product/featured-product/featured-product.effect";
+import { FeaturedProductFeature } from "../../store/product/featured-product/featured-product.reducer";
 
 export const route: Routes = [
     {
         path: '',
         component: LandingComponent,
+        providers: [
+            provideState(FeaturedProductFeature),
+            provideEffects(FeaturedProductEffect),
+        ]
     }
 ]
