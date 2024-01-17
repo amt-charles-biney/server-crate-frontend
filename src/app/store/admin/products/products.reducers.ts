f97fd77b2dea91bb623a516c1c81b09fb4dc3f60
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-16 20:39:54 -1200

    fix: add is featured to product

---
+++ src/app/store/admin/products/products.reducers.ts
@@ -0,0 +1,47 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { AllProducts, ProductItem } from './../../../types';
+import { gotProduct, gotProducts } from './categories.actions';
+import { getUniqueId } from '../../../core/utils/settings';
+const initialState: { products: ProductItem[]; product: ProductItem; total: number } = {
+  products: [],
+  total: 0,
+  product: {
+    productName: '',
+    productDescription: '',
+    productPrice: '',
+    productId: `${getUniqueId(2)}`,
+    productBrand: '',
+    id: '',
+    imageUrl: '',
+    coverImage: '',
+    inStock: 0,
+    sales: 0,
+    category: {
+      name: '',
+      id: '',
+    },
+    isFeatured: false
+  },
+};
+
+export const productsFeature = createFeature({
+  name: 'products',
+  reducer: createReducer(
+    initialState,
+    on(gotProducts, (state, { products }) => ({
+      ...state,
+      products: [...products.products],
+      total: products.total
+    })),
+    on(gotProduct, (state, product) => ({ ...state, product }))
+  ),
+});
+
+export const {
+  name,
+  reducer,
+  selectProducts,
+  selectProduct,
+  selectProductsState,
+  selectTotal
+} = productsFeature;
--- src/app/types.ts
+++ src/app/types.ts
@@ -112,6 +112,7 @@ export type ProductItem = {
     id: string;
     name: string;
   }
+  isFeatured: boolean
 };
 
 export type ResendOtp = {
