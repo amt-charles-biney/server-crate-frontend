f67d0d7583c105eba2ae86b8ba929cad711ea122
Abdul-Raziq <abdul-raziq.baba@gmail.com>
2024-01-18 15:56:03 +0000

    refactor: deleted patterns.ts

--- src/app/core/utils/constants/patterns.ts
+++
@@ -1,6 +0,0 @@
-import { HttpContextToken } from "@angular/common/http";
-
-export const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
-export const UPLOAD_PRESET = 'n6gwltfs'
-export const CLOUD_NAME = 'dah4l2inx'
-export const NO_AUTH = new HttpContextToken<boolean>(() => false);
