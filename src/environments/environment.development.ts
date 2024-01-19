6f40ca92f1f93df0ea1b8124fe0d5ea650c5f4ae
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-18 23:18:59 -1200

    refactor: changed urls into one base url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,4 @@
 export const environment = {
-    base: 'http://localhost:8080/api/v1/auth',
-    profile: 'http://localhost:8080/api/v1/profile',
-    categories: 'http://localhost:8080/api/v1/admin/category',
-    brands: 'http://localhost:8080/api/v1/admin/brand',
-    products: 'http://localhost:8080/api/v1/admin/product', 
-    products_user: 'http://localhost:8080/api/v1/product',
-    category_user: 'http://localhost:8080/api/v1/category',
-    config: 'http://localhost:8080/api/v1/config',
     production: false,
     base_url: 'http://localhost:8080/api/v1', 
 };
