cb38aed8a2438999948d7fd26c878b98f8406c70
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-18 22:34:36 -1200

    feat: added product config item to app route on product config path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState } from '@ngrx/store';
+import { productConfigFeature, productConfigItemFeature } from './store/product-spec/product-spec.reducer';
 import { productConfigFeature } from './store/product-spec/product-spec.reducer';
 import { ProductSpecEffects } from './store/product-spec/product-spec.effect';
 
@@ -10,7 +11,8 @@ export const routes: Routes = [
         loadChildren: () => import('./features/product-configure/product-configure.routes').then(m => m.route),
         providers: [
             provideState(productConfigFeature),
-            provideEffects(ProductSpecEffects)
+            provideEffects(ProductSpecEffects),
+            provideState(productConfigItemFeature)
         ]
     },
 
