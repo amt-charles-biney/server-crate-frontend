ef9a0c4348b226c192065984f962f5f2266842a5
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-18 22:39:14 -1200

    feat: updated the product configure component and component ts to reflect the product configurations

--- src/app/features/product-configure/product-configure.component.ts
+++ src/app/features/product-configure/product-configure.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
 import { FooterComponent } from '../../core/components/footer/footer.component';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationExtras,
+  Router,
+  RouterModule,
+} from '@angular/router';
 import {
   ICategoryConfig,
+  ICategoryOption,
   ICompatibleOption,
+  IConfiguredOption,
+  IConfiguredProduct,
+  IParamConfigOptions,
   IProductConfiguration,
   IProductConfigureOptionType,
   ProductItem,
@@ -13,10 +22,13 @@ import { Store } from '@ngrx/store';
 import {
   selectProduct,
   selectProductConfig,
+  selectProductConfigItem,
+  selectProductConfigItemLoading,
 } from '../../store/product-spec/product-spec.reducer';
 import { Observable, map } from 'rxjs';
 import {
   loadProduct,
+  loadProductConfigItem,
 } from '../../store/product-spec/product-spec.action';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -25,144 +37,152 @@ import cloneDeep from 'lodash.clonedeep';
 @Component({
   selector: 'app-product-configure',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, CommonModule, MatTabsModule],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+    MatTabsModule,
+    RouterModule,
+  ],
   templateUrl: './product-configure.component.html',
   styleUrl: './product-configure.component.scss',
 })
 export class ProductConfigureComponent {
-  product$!: Observable<ProductItem | null>;
-  productConfig!: Observable<any>;
 
-  productConfigInstance!: ICategoryConfig | any;
-  product!: ProductItem;
-  productConfigItem!: IProductConfiguration;
+  @ViewChild('selectConfig') selectConfig!: ElementRef;
+  @ViewChild('selectSize') selectSize!: ElementRef;
+
+
+  product$: Observable<ProductItem | null> =  this.store.select(selectProduct);
+  productConfig$: Observable<any> =  this.store.select(selectProductConfig);
+  productConfigItem$: Observable<IConfiguredProduct | null> =  this.store.select(selectProductConfigItem);
+  loading$: Observable<boolean> = this.store.select(selectProductConfigItemLoading)
+  
 
   productId: string = '';
+  productConfig!: ICategoryConfig;
+  product!: ProductItem;
   warranty: boolean = false;
-  activeLink: string = '';
+  loading: boolean = false
+  size: string = '0'
+  componentSizable: string = ""
 
+  productConfigItem: IConfiguredProduct = {
+    totalPrice: 0,
+    configuredPrice: 0,
+    configured: [],
+    productId: '',
+    productPrice: 0,
+    id: null,
+    warranty: false,
+    vatIncluded: 0,
+  };
+
+  activeLink: string = '';
   configKeys: string[] = [];
+  queryMapper: { [key: string]: string } = {};
+  isComponentSizableQueryMapper: ({[key: string]: string}) = {}
+
+  setActiveLink = (active: string) => (this.activeLink = active);
+$: any;
 
   constructor(
     private route: ActivatedRoute,
     private store: Store,
-    private router: Router
-  ) {}
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
     this.productId = this.route.snapshot.paramMap.get('id')?.toString() || '';
-    this.product$ = this.store.select(selectProduct);
-    this.productConfig = this.store.select(selectProductConfig);
 
     this.store.dispatch(loadProduct({ id: this.productId }));
+
+    this.productConfigItem$.subscribe((product) => {
+      if (product) this.productConfigItem = product;
+      this.buildQueryMapper()
+      this.cdr.detectChanges()
+    });
+
+    this.productConfig$.subscribe((product) => {
+      if (product) this.productConfig = product;
+    });
+
+    this.loading$.subscribe(loader => {
+       this.loading = loader;
+    })
+
+    this.route.queryParams.subscribe((queryParams) => {
+      const configOptions: IParamConfigOptions = {
+        warranty: queryParams['warranty'] || this.warranty,
+        components: queryParams['components']
+      };
+      this.store.dispatch(loadProductConfigItem({ productId: this.productId, configOptions }));
+    });
 
     this.getConfigKeys().subscribe((keys) => {
       this.configKeys = keys;
       this.activeLink = keys[0];
     });
-
-    this.product$.subscribe((prod) => {
-      if (prod) {
-        this.product = cloneDeep(prod);
-        this.productConfigItem = this.transformToProductConfiguration(prod);
-      }
-    });
-
-    this.productConfig.subscribe((product) => {
-      if (product) {
-        this.productConfigInstance = cloneDeep(product);
-        this.productConfigItem = {
-          ...this.productConfigItem,
-          configurations: this.transformToDefaultCategoryConfigOptions(product),
-        };
-        this.updateProductPrice(this.productConfigItem);
-      }
-    });
   }
-
-  transformToProductConfiguration(product: ProductItem): IProductConfiguration {
-    const configuration: IProductConfiguration = {
-      productId: product.productId,
-      configurations: [],
-      productPrice: parseFloat(product.productPrice),
-      configurationPrice: 0,
-      totalPrice: parseFloat(product.productPrice),
-      warrantyType: this.warranty,
-      vatIncluded: 0,
-    };
-
-    return configuration;
-  }
-
-  transformToDefaultCategoryConfigOptions(
-    productConfig: ICategoryConfig
-  ): IProductConfigureOptionType[] {
-    const defaultConfigOptions: IProductConfigureOptionType[] = [];
-
-    for (const key in productConfig.options) {
-      if (productConfig.options.hasOwnProperty(key)) {
-        const optionsArray: ICompatibleOption[] = productConfig.options[key];
-
-        optionsArray.forEach((option) => {
-          if (option.isIncluded) {
-            const configOption: IProductConfigureOptionType = {
-              name: option.name,
-              attribute: option.type,
-              price: option.price,
-            };
-            defaultConfigOptions.push(configOption);
-          }
-        });
-      }
-    }
-
-    return defaultConfigOptions;
-  }
-
-  updateProductPrice = (product: IProductConfiguration): void => {
-    const configurationPrice = product?.configurations.reduce(
-      (accum, val: IProductConfigureOptionType) =>
-        (accum += Number(val?.price)),
-      0
-    );
-    const totalPrice = Number(product?.productPrice || 0) + configurationPrice;
-
-    this.productConfigItem = {
-      ...this.productConfigItem,
-      totalPrice,
-      configurationPrice,
-    };
-  };
-
-  gotoProducts = () => {
-    this.router.navigate(['/product'], { replaceUrl: true });
-  };
-  gotoHome = (): void => {
-    this.router.navigate(['/'], { replaceUrl: true });
-  };
-
-  setActiveLink = (active: string) => {
-    this.activeLink = active;
-  };
 
   getConfigKeys = (): Observable<string[]> => {
-    return this.productConfig.pipe(
+    return this.productConfig$.pipe(
       map((config: any) => Object.keys(config?.options || {}))
     );
   };
 
-  swapImage = (imgtoSwap: string, imageIdx: number) => {
-    let updatedProduct: any = { ...this.product };
-    let previousCoverImage = updatedProduct.coverImage;
-    updatedProduct.coverImage = imgtoSwap;
-    if (updatedProduct.imageUrl && updatedProduct.imageUrl[imageIdx]) {
-      updatedProduct.imageUrl[imageIdx] = previousCoverImage;
-    }
-
-    this.product = { ...updatedProduct };
-  };
-
   onOptionChange(check: boolean) {
     this.warranty = check;
+    this.updateConfigQueryParam(null, this.warranty)
   }
+
+  buildQueryMapper = () => {
+    for (let product of this.productConfigItem.configured) {
+      this.queryMapper[product.optionType] = `${product.optionId}_${product.isMeasured ? product?.size || product?.baseAmount : product?.baseAmount}`;
+    }
+  }
+  
+
+  onSizeableOptionChange = ({ type, id = this.componentSizable, size = this.size }: { type: string, id: string, size: string }) => {
+    this.componentSizable = id;
+    this.size = size;
+  
+    this.updateConfigQueryParam({ type, id: id, size: size });
+  };
+  
+
+
+  updateConfigQueryParam = ( configItem: { type: string; id: string; size: string } | null, warranty = this.warranty): void => {
+    if (configItem) this.queryMapper[configItem.type] = `${configItem.id}_${configItem.size}`;
+    
+    const joinQuery = Object.values(this.queryMapper).join(',');
+    const currentParams = { ...this.route.snapshot.queryParams, warranty, components: joinQuery };
+    
+    const navigationExtras: NavigationExtras = { queryParams: currentParams, queryParamsHandling: 'merge' };
+    
+    this.router.navigate([], navigationExtras);
+  };
+
+
+isActiveSelectedOption = ({ type, id, size }: { type: string; id: string; size: string }): boolean => this.queryMapper[type] === `${id}_${size}`;
+
+generateStorageSizes(productArr: any[]): number[] {
+    let storageSize = [];
+    const minBaseAmount = productArr.reduce((min, option) => Math.min(min, option.baseAmount), Infinity);
+    const maxMaxAmount = productArr.reduce((max, option) => Math.max(max, option.maxAmount), -Infinity);
+    const stepMultiplier = 2;
+  
+    for (let size = minBaseAmount; size <= maxMaxAmount; size *= stepMultiplier) {
+      storageSize.push(size);
+    }
+  
+    return storageSize;
+  }
+
+  resetDefault(type: string) {
+    const getIncludedProduct = this.productConfig.options[type].filter(product => product.isIncluded == true)[0]
+    if(getIncludedProduct) this.updateConfigQueryParam({ type, id: getIncludedProduct?.id, size: getIncludedProduct?.baseAmount.toString()})
+  }
+  
 }
