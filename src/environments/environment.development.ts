3d573030e0cfcc9bb4ab7d52323d9561999aeb27
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-18 22:55:02 -1200

    refactor: added a comma to make environment work

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,7 +6,7 @@ export const environment = {
     products: 'http://localhost:8080/api/v1/admin/product', 
     products_user: 'http://localhost:8080/api/v1/product',
     category_user: 'http://localhost:8080/api/v1/category',
-    config: 'http://localhost:8080/api/v1/config'
+    config: 'http://localhost:8080/api/v1/config',
     production: false,
     base_url: 'http://localhost:8080/api/v1', 
 };
