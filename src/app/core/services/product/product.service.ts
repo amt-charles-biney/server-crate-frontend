7147b1482528252c39ac4f99f426b493fef16d95
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-18 22:33:33 -1200

    feat: updated service to get product config item

--- src/app/core/services/product/product.service.ts
+++ src/app/core/services/product/product.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ProductItem } from '../../../types';
+import { IParamConfigOptions, ProductItem } from '../../../types';
 import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
@@ -10,6 +10,7 @@ export class ProductService {
 
   productUserUrl: string = environment.products_user
   categoryUserUrl: string = environment.category_user
+  configUrl: string = environment.config
 
   constructor(private http: HttpClient) { }
 
@@ -19,5 +20,15 @@ export class ProductService {
 
   getProductConfiguration(categoryId: string) {
     return this.http.get<any>(`${this.categoryUserUrl}/${categoryId}/config`)
+  }
+
+  getProductConfigItem(productId: string, configOptions: IParamConfigOptions) {
+    const url = `${this.configUrl}/${productId}`;
+    
+    let params = new HttpParams();
+    params = params.set('warranty', configOptions.warranty.toString());
+    params = params.set('components', configOptions.components?.toString() || '');
+
+    return this.http.get<any>(url, { params });
   }
 }
