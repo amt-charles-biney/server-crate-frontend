7134283c23c40906ce907297017afed115976b6a
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-02-04 16:50:31 +0000

    fix: fix environment link

--- src/app/core/services/product/product.service.ts
+++ src/app/core/services/product/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IParamConfigOptions, ProductItem } from '../../../types';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../../environments/environment.development';
 import { Observable } from 'rxjs';
 
 @Injectable({
