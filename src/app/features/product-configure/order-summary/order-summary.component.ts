800ae904af2244169ca16283c050acb7efe4bd2f
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-02-04 16:43:11 +0000

    feat: added order summary

--- src/app/features/product-configure/order-summary/order-summary.component.ts
+++ src/app/features/product-configure/order-summary/order-summary.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core'
 import { type IParamConfigOptions, type IConfiguredProduct } from '../../../types'
+import { Store } from '@ngrx/store'
+import { addToCartItem } from '../../../store/product-spec/product-spec.action'
 
 @Component({
   selector: 'app-order-summary',
@@ -11,11 +13,15 @@ export class OrderSummaryComponent {
   @Input() productConfigItem!: IConfiguredProduct
   @Input() warranty: any = false
   @Input() components!: (Record<string, string>)
+  @Input() productId!: string
+
+  constructor (private store: Store) {}
 
   addProductToCart (): void {
     const queryComponents = Object.values(this.components).join(',')
     const configOptions: IParamConfigOptions = { warranty: this.warranty, components: queryComponents }
 
     console.log('config options ', configOptions)
+    this.store.dispatch(addToCartItem({ productId: this.productId, configOptions }))
   }
 }
