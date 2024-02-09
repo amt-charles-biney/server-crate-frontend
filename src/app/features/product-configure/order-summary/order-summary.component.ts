e41ee39386407041c97a3f0eca6079fa0ddadf51
Abdul-Raziq <abdul-raziq.baba@gmail.com>
2024-02-09 09:05:22 +0000

    style: restyled loader component

--- src/app/features/product-configure/order-summary/order-summary.component.ts
+++ src/app/features/product-configure/order-summary/order-summary.component.ts
@@ -22,6 +22,7 @@ export class OrderSummaryComponent {
   constructor (private store: Store) {}
 
   addProductToCart (): void {
+    scrollTo({ top: 0, behavior: 'smooth' })
     this.store.dispatch(addToCartItem({ productId: this.productId, configOptions: this.querySnapShot  }))
   }
 }
