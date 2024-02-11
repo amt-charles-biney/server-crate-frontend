ee145f9148253aad9404ae915e4662f3ed7ef5a7
Abdul-Raziq <abdul-raziq.baba@gmail.com>
2024-02-11 12:14:05 +0000

    feat: Added cookie consent popup

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,11 +1,12 @@
 import { HttpContextToken } from "@angular/common/http";
 
 export const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
-export const NO_AUTH = new HttpContextToken<boolean>(() => false);
-export const OTP_EXPIRATION = 'server-crate-otp-expiration'
 export const UPLOAD_PRESET = 'n6gwltfs'
 export const CLOUD_NAME = 'dah4l2inx'
+export const NO_AUTH = new HttpContextToken<boolean>(() => false);
 
-export const CURRENT_AD_TAB = 'server-crate-current-ad-tab'
 export const CURRENT_INDEX = 'server-crate-current-index'
 export const CURRENT_AD_INDEX = 'server-crate-current-ad-index'
+export const CURRENT_AD_TAB = 'server-crate-current-ad-tab'
+export const OTP_EXPIRATION = 'server-crate-otp-expiration'
+export const COOKIE_ACCEPTANCE = 'server-crate-cookie-acceptance'
