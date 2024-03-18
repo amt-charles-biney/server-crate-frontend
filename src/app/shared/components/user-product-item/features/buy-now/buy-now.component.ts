38caeedbc040e6a89983587e20b4d4a33872e1f3
Abdul-Raziq1 <abdul-raziq.baba@amalitech.com>
2024-03-18 10:12:49 +0000

    style: restyled configure page configuration products

---
+++ src/app/shared/components/user-product-item/features/buy-now/buy-now.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-now',
+  standalone: true,
+  imports: [],
+  templateUrl: './buy-now.component.html',
+})
+export class BuyNowComponent {
+
+}
