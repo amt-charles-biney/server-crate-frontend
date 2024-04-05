352aa3825d816ed96f8e5244405d5724035b2cbf
Abdul-Raziq1 <abdul-raziq.baba@amalitech.com>
2024-04-05 04:34:36 +0000

    feat: search occurs when typing

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import { CustomButtonComponent } from '../../../shared/components/custom-button/custom-button.component';
@@ -11,15 +11,22 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatBadgeModule} from '@angular/material/badge';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { BehaviorSubject, Observable, debounceTime, tap } from 'rxjs';
 import { selectCount } from '../../../store/cart/cart.reducers';
 import { MegaMenuComponent } from './mega-menu/mega-menu.component';
 import { selectTotalElements } from '../../../store/admin/products/wishlist.reducers';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-header',
@@ -38,60 +45,67 @@ import { selectTotalElements } from '../../../store/admin/products/wishlist.redu
     MatBadgeModule,
   ],
   templateUrl: './header.component.html',
-  animations: [
-    trigger('showSearch', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('.3s ease-in', style({ opacity: 1 })),
-      ]),
-      
-    ]),
-    trigger('hideSearch', [
-      transition(':leave', [
-        style({ opacity: 1 }),
-        animate('.3s ease-in', style({ opacity: 0 })),
-      ]),
-    ]),
-    trigger('openClose', [
-      state('true', style({ height: '*' })),
-      state('false', style({ height: '64px' })),
-      transition('false <=> true', animate(200))
-    ])
-  ],
 })
 export class HeaderComponent implements OnInit {
   isMegaMenu: boolean = false;
   isAuthenticated!: boolean;
   showSearch: boolean = false;
-  searchForm!: FormGroup
-  numberOfCartItems$!: Observable<number>
-  numberOfWishlistItems$!: Observable<number>
-  constructor(private authService: AuthService, private store: Store, private router: Router, private renderer: Renderer2) {
+  searchForm!: FormGroup;
+  numberOfCartItems$!: Observable<number>;
+  numberOfWishlistItems$!: Observable<number>;
+  constructor(
+    private authService: AuthService,
+    private store: Store,
+    private router: Router,
+    private destroyRef: DestroyRef
+  ) {
     this.isAuthenticated = this.authService.isAuthenticated();
   }
   ngOnInit(): void {
     this.searchForm = new FormGroup({
-      searchValue: new FormControl('', Validators.required)
-    })
-    this.numberOfCartItems$ = this.store.select(selectCount) 
-    this.numberOfWishlistItems$ = this.store.select(selectTotalElements)
+      searchValue: new FormControl('', Validators.required),
+    });
+    this.numberOfCartItems$ = this.store.select(selectCount);
+    this.numberOfWishlistItems$ = this.store.select(selectTotalElements);
+    this.searchValue.valueChanges
+      .pipe(
+        debounceTime(900),
+        tap((value) => {
+          console.log('Value', value);
+
+          if (value) {
+            this.router.navigate(['/servers'], {
+              queryParams: { query: `${value}` },
+            });
+          } else if (value === '') {
+            this.router.navigate(['/servers'], { replaceUrl: true });
+          }
+          
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
   }
 
   openSearch() {
     this.showSearch = !this.showSearch;
+    if (!this.showSearch) {
+      this.searchForm.patchValue({ searchValue: '' })
+    }
   }
 
   search() {
-    if (this.searchForm.invalid) return ;
-    this.router.navigate(['/servers'], { queryParams: { query: `${ this.searchValue.value }`} })
+    if (this.searchForm.invalid) return;
+    this.router.navigate(['/servers'], {
+      queryParams: { query: `${this.searchValue.value}` },
+    });
   }
 
   get searchValue() {
-    return this.searchForm.get('searchValue')!
+    return this.searchForm.get('searchValue')!;
   }
 
-
   toggleMenu(menuState: boolean) {
-    this.isMegaMenu = menuState
+    this.isMegaMenu = menuState;
   }
 }
